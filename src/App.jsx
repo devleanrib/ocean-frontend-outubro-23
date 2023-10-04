7123fbd28543b665d881b1c8b0aa536dad070ac6
Leandro Ribeiro <133710021+devleanrib@users.noreply.github.com>
2023-10-04 14:08:16 +0000

    atualização codigo

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+App.jsx está assim
+
+import './App.css'
+
 function App() {
   return (
     <>
-        <div>
-          <h2>Rick Sanchez</h2>
-          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
-        </div>
-        </>
+    <div>
+    <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    </div>
+    </>
   )
 }
-
 
 export default App
